8292d6d85a06b1d382527cfc49f6506ea2f1bd93
NampallyHarshini <121306410+NampallyHarshini@users.noreply.github.com>
2022-12-27 20:12:44 +0530

    Create index.js

---
+++ src/Components/Placements/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./index.css";
+
+const Placements = () => {
+  return (
+    <div className="placements-container p-3 p-md-5">
+      <h1 className="text-center mt-5">100% Placements Assistance</h1>
+      <h2>Resume Building</h2>
+      <p>
+        1FIN provides professional guidance to help you create an effective
+        resume to help you create a lasting impression.
+      </p>
+      <h2>Career Counselling</h2>
+      <p>
+        We provide career counselling to conducting mock interviews and aptitude
+        tests – we will assist you at every step, all the way, we make you job
+        ready
+      </p>
+      <h2>Jobs</h2>
+      <p className="mb-5">
+        We will give you an edge over others with our direct corporate
+        affiliations which will ensure that you are placed right.
+      </p>
+    </div>
+  );
+};
+
+export default Placements;
