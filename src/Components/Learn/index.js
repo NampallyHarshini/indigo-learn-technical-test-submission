024bbfb74348d8218bde4055844ed36371db8b4c
NampallyHarshini <121306410+NampallyHarshini@users.noreply.github.com>
2022-12-27 19:20:35 +0530

    Create index.js

---
+++ src/Components/Learn/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import "./index.css";
+
+const Learn = () => {
+  return (
+    <div className="learn-container w-100 row d-flex justify-content-center pt-5 pb-5">
+      <div className="col-12 col-md-6 col-lg-3 text-center shadow learn-item">
+        <div className="learn-header d-flex justify-content-center align-items-center">
+          <h1 className="text-center">Knowledge Level</h1>
+        </div>
+        <ul className="learn-main">
+          <li>Business and Technology (BT)</li>
+          <li>Management Accounting (MA)</li>
+          <li>Financial Accounting (FA)</li>
+        </ul>
+        <div className="learn-footer d-flex justify-content-center align-items-center">
+          <h1 className="text-center">3 Papers</h1>
+        </div>
+      </div>
+      <div className="col-12 col-md-6 col-lg-3 text-center shadow learn-item">
+        <div className="learn-header d-flex justify-content-center align-items-center">
+          <h1 className="text-center">Skill Lever</h1>
+        </div>
+        <ul className="learn-main">
+          <li>Corporate and Business Law (LW)</li>
+          <li>Performance Management (PM)</li>
+          <li>Taxation (TX)</li>
+          <li>Financial Reporting (FR)</li>
+          <li>Audit and Assurance (AA)</li>
+          <li>Financial Management (FM)</li>
+        </ul>
+        <div className="learn-footer d-flex justify-content-center align-items-center">
+          <h1 className="text-center">6 Papers</h1>
+        </div>
+      </div>
+      <div className="col-12 col-md-6 col-lg-3 text-center shadow learn-item">
+        <div className="learn-header d-flex justify-content-center align-items-center">
+          <h1 className="text-center">Knowledge Level</h1>
+        </div>
+        <ul className="learn-main">
+          <li>Business and Technology (BT)</li>
+          <li>Management Accounting (MA)</li>
+          <li>Financial Accounting (FA)</li>
+        </ul>
+        <div className="learn-footer d-flex justify-content-center align-items-center">
+          <h1 className="text-center">3 Papers</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Learn;
